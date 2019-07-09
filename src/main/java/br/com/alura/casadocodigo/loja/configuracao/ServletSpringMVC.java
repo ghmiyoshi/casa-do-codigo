7c5cc1ea7d56f7ea93e9dffd6b6434b23fb06afb
unknown <gabriel_hidekii@hotmail.com>
2019-07-08 22:40:09 -0300

    Preparando Controller, DAO e JSP Listar

--- src/main/java/br/com/alura/casadocodigo/loja/configuracao/ServletSpringMVC.java
+++ src/main/java/br/com/alura/casadocodigo/loja/configuracao/ServletSpringMVC.java
@@ -1,5 +1,8 @@
 package br.com.alura.casadocodigo.loja.configuracao;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 // Servlet de configuração do SpringMVC para atender as requisições
@@ -18,6 +21,16 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" }; // Defini a URL que o Spring deve mapear / para frente
+	}
+	
+	// Registrando filtro para resolver erros de encoding
+	// Toda requisição que chegar para o Spring ele transforma para UTF-8 
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		
+		return new Filter[] {characterEncodingFilter}; 
 	}
 
 }
--- src/main/java/br/com/alura/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/alura/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,8 +1,11 @@
 package br.com.alura.casadocodigo.loja.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.alura.casadocodigo.loja.daos.ProdutoDAO;
@@ -12,12 +15,14 @@ import br.com.alura.casadocodigo.loja.models.TipoPreco;
 @Controller
 public class ProdutosController {
 
-	@Autowired // Pede para o Spring uma instância desse objeto que foi anotado (Injeção de dependência)
+	@Autowired // Pede para o Spring uma instância desse objeto que foi anotado (Injeção de
+				// dependência)
 	private ProdutoDAO produtoDao;
 
 	@RequestMapping("/produtos/form")
 	public ModelAndView form() {
-		// ModelAndView - Além de retornar uma página, possibilita enviar objetos de qualquer classe para essas páginas
+		// ModelAndView - Além de retornar uma página, possibilita enviar objetos de
+		// qualquer classe para essas páginas
 		ModelAndView modelAndView = new ModelAndView("produtos/form");
 		modelAndView.addObject("tipos", TipoPreco.values());
 		System.out.println("Acessando form de produtos");
@@ -25,11 +30,20 @@ public class ProdutosController {
 		return modelAndView;
 	}
 
-	@RequestMapping("/produtos")
+	@RequestMapping(value = "/produtos", method = RequestMethod.POST) // Method POST para gravar
 	public String grava(Produto produto) {
 		System.out.println(produto);
 		produtoDao.gravar(produto);
 		return "ok";
+	}
+
+	@RequestMapping(value = "/produtos", method = RequestMethod.GET) // Method GET para listar
+	public ModelAndView listar() {
+		List<Produto> produtos = produtoDao.listar();
+		ModelAndView modelAndView = new ModelAndView("produtos/lista");
+		modelAndView.addObject("produtos", produtos);
+
+		return modelAndView;
 	}
 
 }
